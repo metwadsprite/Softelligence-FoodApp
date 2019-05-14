4a5df799353e3fa3ce1bbae9061ba7dfff32b394
Catalin Sbora <catalin.sbora@gmail.com>
2019-05-14 14:05:25 +0300

    - add support for sessions

---
+++ SoftelligenceFoodApp/DataAccess/EF.DataAccess/DataModel/SessionDO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EF.DataAccess.DataModel
+{
+    [Table("Sessions")]
+    public class SessionDO
+    {
+    }
+}
