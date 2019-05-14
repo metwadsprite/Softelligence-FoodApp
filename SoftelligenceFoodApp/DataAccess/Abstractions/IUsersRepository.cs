99cb37bf4f604b583e48acccc8f403e9d0ee9133
Daniel <danielbouleanu125@yahoo.com>
2019-05-14 17:01:35 +0300

    - implement User Repository Interface

--- SoftelligenceFoodApp/DataAccess/Abstractions/IUsersRepository.cs
+++ SoftelligenceFoodApp/DataAccess/Abstractions/IUsersRepository.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Abstractions
 {
-    interface IUsersRepository
+    public interface IUsersRepository
     {
+        User GetById();
+        String GetEmail();
+        String GetName();
+        ICollection<Order> GetHistory();
+        void PlaceOrder();
+        void ChangeOrder();
+        void CancelOrder();
+        void ViewOrder();
     }
 }
