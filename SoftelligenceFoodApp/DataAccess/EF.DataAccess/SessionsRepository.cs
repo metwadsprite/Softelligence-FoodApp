3c181dfcdedaf2a91b4fb479dc7a6eb2179bb98a
Daniel <danielbouleanu125@yahoo.com>
2019-05-16 14:58:03 +0300

    -add userservice implementations

--- SoftelligenceFoodApp/DataAccess/EF.DataAccess/SessionsRepository.cs
+++ SoftelligenceFoodApp/DataAccess/EF.DataAccess/SessionsRepository.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BusinessLogic;
+using BusinessLogic.Abstractions;
 
 namespace EF.DataAccess
 {
-    class SessionsRepository
+    class SessionsRepository : ISessionsRepository
     {
+        public void Create(Session sessionToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Session GetActiveSession()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
