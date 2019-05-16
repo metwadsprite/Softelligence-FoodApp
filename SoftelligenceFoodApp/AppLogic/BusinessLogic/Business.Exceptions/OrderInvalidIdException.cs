320301ed3ea18917fa77bbfd2c3ca2ed4920e8f7
Vlad Vasilescu <vladvas98@yahoo.com>
2019-05-16 14:24:49 +0300

    implement user logic 😂😂😂👀✔

---
+++ SoftelligenceFoodApp/AppLogic/BusinessLogic/Business.Exceptions/OrderInvalidIdException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BusinessLogic.BusinessExceptions
+{
+    [System.Serializable]
+    public class OrderInvalidIdException : Exception
+    {
+        public OrderInvalidIdException()
+        {
+        }
+        public OrderInvalidIdException(string message) : base(message)
+        {
+        }
+        public OrderInvalidIdException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+        protected OrderInvalidIdException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
