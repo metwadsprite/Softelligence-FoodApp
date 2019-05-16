7b09649586e3cb850c26e6c23336c77831a87e7d
Daniel <danielbouleanu125@yahoo.com>
2019-05-16 14:26:43 +0300

    - add exception user not found

--- SoftelligenceFoodApp/AppLogic/App.Services/UserService.cs
+++ SoftelligenceFoodApp/AppLogic/App.Services/UserService.cs
@@ -5,6 +5,29 @@ namespace Logic.Implementations
 {
     public class UserService
     {
-        
+        private User user;
+        private readonly IPersistenceContext dataContext;
+
+        public UserService(IPersistenceContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public void SelectCurrentUser(int userId)
+        {
+            this.user = dataContext.GetUsersRepository().GetById(userId);
+        }
+
+        public void GetActiveSession() { }
+        //+inf order
+        public void PlaceOrder() { }
+        //order info to change
+        public void ChangeOrder() { }
+        public void CancelOrder() { }
+        public Order GetCurrentOrder()
+        {
+            user.
+            return null;
+        }
     }
 }
---
+++ SoftelligenceFoodApp/AppLogic/BusinessLogic/Business.Exceptions/UserNotFoundException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace BusinessLogic.Business.Exceptions
+{
+    [System.Serializable]
+    public class UserNotFoundException : Exception
+    {
+
+        public UserNotFoundException()
+        {
+        }
+
+        public UserNotFoundException(string message):base(message)
+        {
+        }
+
+        public UserNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UserNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+    }
+}
