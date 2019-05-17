3dca8ef7dde5d41a4709b1ed3089b6dea6759636
Daniel <danielbouleanu125@yahoo.com>
2019-05-17 14:27:23 +0300

    -mapping for session, order, store

--- SoftelligenceFoodApp/AppLogic/BusinessLogic/Store.cs
+++ SoftelligenceFoodApp/AppLogic/BusinessLogic/Store.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BusinessLogic
 {
@@ -7,5 +8,6 @@ namespace BusinessLogic
         public int Id { get; set; }
         public string Name { get; set; }
         public string Link { get; set; }
+        public ICollection<MenuItem> menuItems { get; set; }
     }
 }
