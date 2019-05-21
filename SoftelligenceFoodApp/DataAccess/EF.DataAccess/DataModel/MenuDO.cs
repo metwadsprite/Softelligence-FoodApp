bfbd09f387c667de0e23697b26d45b9f714457ce
Vlad Vasilescu <vladvas98@yahoo.com>
2019-05-21 15:24:30 +0300

    add identity 👍👌✌

--- SoftelligenceFoodApp/DataAccess/EF.DataAccess/DataModel/MenuDO.cs
+++ SoftelligenceFoodApp/DataAccess/EF.DataAccess/DataModel/MenuDO.cs
@@ -11,6 +11,6 @@ namespace EF.DataAccess.DataModel
         public int Id { get; set; }
         public string Hyperlink { get; set; }
         public string Image { get; set; }
-        public ICollection<MenuItem> MenuItems { get; set; }
+        public ICollection<MenuItemDO> MenuItems { get; set; }
     }
 }
