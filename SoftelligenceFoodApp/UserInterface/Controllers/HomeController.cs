e221edb6308c3db62e8200a7a5c113e9e60933d0
rno27 <ivanescurenato@yahoo.com>
2019-05-21 16:13:37 +0300

    start to implement UI

--- SoftelligenceFoodApp/UserInterface/Controllers/HomeController.cs
+++ SoftelligenceFoodApp/UserInterface/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace UserInterface.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Menu()
         {
             return View();
         }
@@ -19,7 +19,7 @@ namespace UserInterface.Controllers
         {
             return View();
         }
-
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
