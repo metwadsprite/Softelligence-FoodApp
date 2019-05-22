bc831c10246f3905f7c250f9e1620b2f43e6bca4
AlexCirciumaru <43677432+AlexCirciumaru@users.noreply.github.com>
2019-05-22 16:17:37 +0300

    - update store management controller - update startup.cs

--- SoftelligenceFoodApp/UserInterface/Controllers/StoreManagementController.cs
+++ SoftelligenceFoodApp/UserInterface/Controllers/StoreManagementController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLogic.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UserInterface.Controllers
 {
     public class StoreManagementController : Controller
     {
+        private IStoresRepository storeRepo;
+
+        public StoreManagementController(IPersistenceContext dataContext)
+        {
+            storeRepo = dataContext.GetStoresRepository();
+        }
+
         public IActionResult Index()
-        {            
-            return View();
+        {
+            var storeList = storeRepo.GetAll();
+            return View(storeList);
         }
 
         public IActionResult Add()
