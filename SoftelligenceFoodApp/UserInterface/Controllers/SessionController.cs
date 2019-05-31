c017c6748459a143fb074842b8dd586ab5d12409
AlexCirciumaru <43677432+AlexCirciumaru@users.noreply.github.com>
2019-05-31 14:27:05 +0300

    - implement add restaurant in session with checkbox - add css and js resources (font awesome) - fixed update store bug - made store management page responsive

--- SoftelligenceFoodApp/UserInterface/Controllers/SessionController.cs
+++ SoftelligenceFoodApp/UserInterface/Controllers/SessionController.cs
@@ -2,10 +2,10 @@
 using Logic.Implementations;
 using Microsoft.AspNetCore.Mvc;
 using UserInterface.Models;
-using BusinessLogic;
-using System.Collections.Generic;
 using BusinessLogic.BusinessExceptions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UserInterface.Controllers
 {
@@ -40,36 +40,46 @@ namespace UserInterface.Controllers
             {
                 session.Session = adminService.GetActiveSession();
                 session.HasActiveSession = true;
-                session.Stores = session.Session.Stores;
+                session.Stores = session.Session.Stores
+                                                .ToList();
             }
-            catch(SessionNotFoundException)
+            catch (SessionNotFoundException)
             {
                 session.HasActiveSession = false;
-                session.Stores = adminService.GetAllStores();
+                session.Stores = adminService.GetAllStores()
+                                            .ToList();
             }
             return View(session);
 
         }
+
         [HttpGet]
         public IActionResult Details(int? id)
         {
             Session session = adminService.GetSessionById(id.Value);
             return View(session);
         }
+
         [HttpPost]
-        public IActionResult Create([FromForm]Session newSession)
+        public IActionResult Create([FromForm]SessionVM newSession)
         {
             if (ModelState.IsValid)
-            {
-                newSession.StartTime = DateTime.Now;
-                var store1 = adminService.GetStoreById(1);
-                var store2 = adminService.GetStoreById(10);
-                newSession.AddStore(store1);
-                newSession.AddStore(store2);
-                adminService.StartSession(newSession);
+            {                
+                Session sessionToCreate = new Session();
+                sessionToCreate.StartTime = DateTime.Now;                
+                
+                for(int i = 0; i < newSession.Stores.Count(); i++)
+                {
+                    if(newSession.SelectedStores[i])
+                    {
+                        var currentStore = adminService.GetStoreById(newSession.Stores[i].Id);
+                        sessionToCreate.AddStore(currentStore);
+                    }
+                  
+                }
+                adminService.StartSession(sessionToCreate);
             }
             return RedirectToAction("NewSession");
         }
-      
     }
 }
--- SoftelligenceFoodApp/UserInterface/Models/SessionVM.cs
+++ SoftelligenceFoodApp/UserInterface/Models/SessionVM.cs
@@ -1,18 +1,21 @@
 ﻿using BusinessLogic;
-using System;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace UserInterface.Models
 {
+    /*public class SelectedStoreEntry
+    {
+        public int StoreId { get; set; }
+        public bool Selected { get; set; }
+    }*/
     public class SessionVM
     {
-        public IEnumerable<Store> Stores { get; set; }
+        public List<Store> Stores { get; set; }
         public Session Session { get; set; }
-        public IEnumerable<User> Users { get; set; }
+        public List<User> Users { get; set; }
         public bool HasActiveSession { get; set; }
-        public decimal Price { get; set; }
-        public ICollection<bool> SelectedStores { get; set; }
+        public decimal Price { get; set; }        
+        public List<bool> SelectedStores { get; set; }
     }
 }
