dbb3fa44379b4817c7e424a9f972c990f0ca8d05
Vlad Vasilescu <vladvas98@yahoo.com>
2019-06-05 11:22:53 +0300

    required login for index

--- SoftelligenceFoodApp/UserInterface/Controllers/HomeController.cs
+++ SoftelligenceFoodApp/UserInterface/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UserInterface.Models;
 
@@ -10,12 +11,8 @@ namespace UserInterface.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
